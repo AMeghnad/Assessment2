b0398768cd94bb768929e8f844c4f7f3af939b8e
AMeghnad <anshumanmeghnad@gmail.com>
2017-11-03 14:52:09 +1100

    Initial upload

---
+++ Assets/Scripts/AllySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assessment2
+{
+    public class AllySpawner : MonoBehaviour
+    {
+        public GameObject[] pillows;
+        public Transform target;
+        public float spawnRate = 1f;
+        public float spawnRadius = 1f;
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+
+        void Spawn()
+        {
+            GameObject clone = Instantiate(pillows[Random.Range(0, pillows.Length)]);
+
+            AIAgent aiAgent = clone.GetComponent<AIAgent>();
+            aiAgent.target = target;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Spawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assessment2
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float health = 100f; // Enemie's health which starts at 100
+
+        public void DealDamage(float damage)
+        {
+            // SET health -= damage
+            health -= damage;
+            // IF health <= 0
+            if (health <= 0)
+            {
+                // Destroy the enemy
+                Destroy(gameObject);
+            }
+        }
+
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.tag == "Pillow")
+            {
+                health = health - 10;
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
